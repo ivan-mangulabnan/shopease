2975a976dfefa1a59a94a6bed7840e98874bae4c
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-28 07:59:27 +0800

    add cart context provider

---
+++ src/cart-context.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+const CartContext = createContext();
+
+function CartContextProvider ({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{cart, setCart}}>
+      { children }
+    </CartContext.Provider>
+  )
+}
+
+export { CartContext };
+export default CartContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
+import CartContextProvider from './cart-context'
 import router from './router'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <CartContextProvider>
+      <RouterProvider router={router}/>
+    </CartContextProvider>
+  </StrictMode>
 )
