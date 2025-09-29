df20f1cad330682a06cbc2de6d46de7da19b51ae
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-29 13:19:29 +0800

    add test if logo link routes to home

--- src/logo/logo.test.jsx
+++ src/logo/logo.test.jsx
@@ -2,7 +2,8 @@ import { describe, expect, it } from "vitest";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import Logo from "./logo.jsx";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, createMemoryRouter, RouterProvider } from "react-router";
+import { routes } from "../router.jsx";
 
 describe('Logo Component', () => {
   it('renders the logo text', () => {
@@ -16,5 +17,19 @@ describe('Logo Component', () => {
     const logo = screen.getByRole('link');
 
     expect(logo).toHaveTextContent(/shopease/i);
+  })
+
+  it('navigates to home when logo is clicked', async () => {
+
+    const router = createMemoryRouter(routes, { initialEntries: ['/shop'] });
+    render(<RouterProvider router={router}/>)
+
+    const logo = screen.getByRole('link', { name: /shopease/i} );
+
+    await userEvent.click(logo);
+
+    const homePage = screen.getByTestId('home-page');
+
+    expect(homePage).toBeInTheDocument();
   })
 })
--- src/router.jsx
+++ src/router.jsx
@@ -18,4 +18,5 @@ const routes = [
 
 const router = createBrowserRouter(routes);
 
+export { routes };
 export default router;
