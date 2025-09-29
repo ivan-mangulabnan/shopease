fef21ebe40ec7cb0f01fddd457657b0673d81f9c
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-29 12:23:43 +0800

    import logo and navbar components

--- src/banner/banner.jsx
+++ src/banner/banner.jsx
@@ -1,7 +1,11 @@
+import Logo from "../logo/logo.jsx";
+import NavBar from "../navbar/navbar.jsx";
+
 function Banner () {
   return (
     <div>
-
+      <Logo />
+      <NavBar />
     </div>
   )
 }
