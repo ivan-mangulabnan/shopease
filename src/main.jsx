a8758617e9030295fdf5cfe88df82e13b16cdb84
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-29 17:58:46 +0800

    setup tanstack queryClient

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import CartContextProvider from './cart-context'
 import router from './router'
 import './index.css'
 
+const query = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CartContextProvider>
-      <RouterProvider router={router}/>
-    </CartContextProvider>
+    <QueryClientProvider>
+      <CartContextProvider>
+        <RouterProvider router={router}/>
+      </CartContextProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
