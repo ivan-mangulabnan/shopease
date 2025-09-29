93a14e3888ecc11f648f78a94e362990cf8acc1f
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-29 19:30:47 +0800

    add outlet for children

--- src/shop/shop.jsx
+++ src/shop/shop.jsx
@@ -1,7 +1,47 @@
+import { Outlet } from "react-router"
+
 function Shop () {
   return (
     <div data-testid='shop-page'>
       I'm Shop!
+      <Outlet />
+    </div>
+  )
+}
+
+
+
+function ProductCard ({ product }) {
+  return (
+    <form>
+      <h2>{product.title}</h2>
+      <p>{product.price}</p>
+    </form>
+  )
+}
+
+const Image = ({ source, altText = '' }) => {
+  return (
+    <div>
+      <img src={source} alt={altText} />
+    </div>
+  )
+}
+
+const ProductButtons = () => {
+  return (
+    <div>
+      <button type='submit'>Add To Cart</button>
+    </div>
+  )
+}
+
+const Quantity = () => {
+  return (
+    <div>
+      <button type='button'>Decrease</button>
+      <input name="quantity" type='text' required/>
+      <button type='button'>Increase</button>
     </div>
   )
 }
