eff5dd193efb7ffb8fb651e924190e1b6ce03525
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-29 19:05:49 +0800

    add index children to shop component

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,9 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import Layout from "./layout/layout";
 import Home from "./home/home";
 import Cart from "./cart/cart";
 import Shop from "./shop/shop";
+import Products from "./products/products";
 
 const routes = [
   {
@@ -10,8 +11,14 @@ const routes = [
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'cart', element: <Cart /> },
-      { path: 'shop', element: <Shop /> }
+      { path: '/cart', element: <Cart /> },
+      { path: '/shop',
+        element: <Shop />,
+        children: [
+          { index: true, element: <Navigate to='shop/products' replace/> },
+          { path: 'shop/products', element: <Products /> }
+        ]
+      }
     ]
   }
 ]
