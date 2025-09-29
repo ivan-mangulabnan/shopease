42a6f81adabe3be00271c5e8662ddaeb5c338068
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-09-29 19:31:36 +0800

    add products component

--- src/products/products.jsx
+++ src/products/products.jsx
@@ -1,10 +1,16 @@
 import useProducts from "../get-product.jsx";
+import ProductCard from "../product-card/card.jsx";
 
 function Products () {
   const { data: products, isError, isLoading, error } = useProducts();
 
+  if (isError) return <p>Error: {error.message}</p>
+  if (isLoading) return <p>Loading...</p>
+
   return (
-    <div data-testid='products'></div>
+    <div data-testid='products'>
+      { products.map(product => <ProductCard key={product.id} product={product} />)}
+    </div>
   )
 }
 
